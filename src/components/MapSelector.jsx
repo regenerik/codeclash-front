80db45ecd4d1eec4a40b2cc683737a7950819a57
regenerik <regenerik@gmail.com>
2024-11-30 14:52:22 -0300

    veracruz fixed 2

--- src/components/MapSelector.jsx
+++ src/components/MapSelector.jsx
@@ -108,7 +108,7 @@ const MapSelector = () => {
             TABASCO: 27,
             TAMAULIPAS: 28,
             TLAXCALA: 29,
-            "VERACRUZ  DE IGNACIO DE LA LLAVE": 30,
+            VERACRUZ: 30,
             YUCATAN: 31,
             ZACATECAS: 32,
         };
